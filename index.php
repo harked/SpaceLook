8536e76e7dc6017c9b7146b7b37519c22ff58868
Randy Harkediansa <randy.harkediansa@gmail.com>
2019-02-26 20:54:36 +0800

    Update Spacelook homepage
    Cloning google homepage

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 <html>
 	<head>
 		<title>Welcome to SpaceLook</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	</head>
 	<body>
-		<div class="mainSection">
-			<div class="logoContainer">
-				<img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-			</div>
+		<div class="wrapper indexPage">
+			<div class="mainSection">
+				<div class="logoContainer">
+					<img src="assets/images/spacelook.png">
+				</div>
 
-			<div class="searchContainer">
-				<form action="search.php" method="GET">
-					<input class="searchBox" type="text" name="term">
-					<input class="searchButton" type="submit" value="Search">
-				</form>
+				<div class="searchContainer">
+					<form action="search.php" method="GET">
+						<input class="searchBox" type="text" name="term">
+						<input class="searchButton" type="submit" value="Search">
+					</form>
+				</div>
 			</div>
 		</div>
 	</body>
